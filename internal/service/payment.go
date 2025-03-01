4e94f6afc8a51de696265ac21194cde51e9b29f2
VinGP <105109338+VinGP@users.noreply.github.com>
2025-03-02 02:07:44 +0300

    fix

--- internal/service/payment.go
+++ internal/service/payment.go
@@ -1,9 +1,11 @@
 package service
 
 import (
+	"fmt"
 	"github.com/rvinnie/yookassa-sdk-go/yookassa"
 	yoocommon "github.com/rvinnie/yookassa-sdk-go/yookassa/common"
 	"github.com/rvinnie/yookassa-sdk-go/yookassa/payment"
+	"math"
 )
 
 type YookassaPayment struct {
@@ -16,12 +18,12 @@ func NewYookassaPayment(accountId string, secretKey string) *YookassaPayment {
 	}
 }
 
-func (p *YookassaPayment) CreatePayment(paymentData map[string]interface{}) (string, error) {
+func (p *YookassaPayment) CreateOrderPayment(orderID int64, amount float64) (string, error) {
 	paymentHandler := yookassa.NewPaymentHandler(p.client)
 	// Создаем платеж
 	payment, _ := paymentHandler.CreatePayment(&yoopayment.Payment{
 		Amount: &yoocommon.Amount{
-			Value:    "1000.00",
+			Value:    fmt.Sprintf("%.2f", math.Round(amount*100)/100),
 			Currency: "RUB",
 		},
 		PaymentMethod: yoopayment.PaymentMethodType("bank_card"),
@@ -30,6 +32,9 @@ func (p *YookassaPayment) CreatePayment(paymentData map[string]interface{}) (str
 			ReturnURL: "https://www.example.com",
 		},
 		Description: "Test payment",
+		Metadata: map[string]interface{}{
+			"order_id": orderID,
+		},
 	})
 
 	return payment.Confirmation.(map[string]interface{})["confirmation_url"].(string), nil
