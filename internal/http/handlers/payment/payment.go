9457ab9ce1cb1d39005f1200d843b228ac5b62ec
VinGP <105109338+VinGP@users.noreply.github.com>
2025-03-02 04:21:14 +0300

    fix

--- internal/http/handlers/payment/payment.go
+++ internal/http/handlers/payment/payment.go
@@ -6,13 +6,15 @@ import (
 	"github.com/RCSE2025/backend-go/pkg/api/response"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type paymentRoutes struct {
-	yookassa *service.YookassaPayment
+	yookassa     *service.YookassaPayment
+	orderService *service.OrderService
 }
 
-func NewProductRoutes(h *gin.RouterGroup, yookassa *service.YookassaPayment) {
+func NewProductRoutes(h *gin.RouterGroup, yookassa *service.YookassaPayment, orderService *service.OrderService) {
 	g := h.Group("/payment")
 
 	pr := paymentRoutes{
@@ -43,29 +45,23 @@ func (pr *paymentRoutes) notification(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusBadRequest, response.Error(err.Error()))
 		return
 	}
-	//fmt.Println(t)
-	//fmt.Println(t["metadata"])
-	//tt := t["metadata"].(map[string]string)
-	//fmt.Println(tt)
-	//fmt.Println(tt["order_id"])
-	////fmt.Println(tt.(int))
-	//var req notification
-	//if err := c.ShouldBindJSON(&req); err != nil {
-	//	c.AbortWithStatusJSON(http.StatusBadRequest, response.Error(err.Error()))
-	//	return
-	//}
-	//fmt.Println(req)
 
 	fmt.Println(t)
 
-	// The metadata is nested inside the "object" field, need to safely access it
 	if object, ok := t["object"].(map[string]interface{}); ok {
 		if metadata, ok := object["metadata"].(map[string]interface{}); ok {
 			if orderID, ok := metadata["order_id"].(string); ok {
 				fmt.Println("Order ID:", orderID)
-				// Process the order ID
-
-				// Return successful response
+				i, err := strconv.ParseInt(orderID, 10, 64)
+				if err != nil {
+					c.AbortWithStatusJSON(http.StatusBadRequest, response.Error(err.Error()))
+					return
+				}
+				err = pr.orderService.ConfirmOrderPayment(i)
+				if err != nil {
+					c.AbortWithStatusJSON(http.StatusBadRequest, response.Error(err.Error()))
+					return
+				}
 				c.Status(http.StatusOK)
 				return
 			}
